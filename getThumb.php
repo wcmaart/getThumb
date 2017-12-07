7b5b3b763494af34dcdc5efdcb1998eb73c2c0be
Chad Weinard <cweinard@gmail.com>
2017-12-07 12:33:16 -0500

    Add files via upload

---
+++ getThumb.php
@@ -0,0 +1,21 @@
+<?php
+$myID =  $_GET["objectid"];
+//echo $myID;
+
+//change $jsonurl to match domain
+$jsonurl = "http://egallery.williams.edu/apis/iiif/presentation/v2/objects-" . $myID . "/manifest";
+
+$json = file_get_contents($jsonurl);
+$decoded = json_decode($json,true);
+
+if (strpos($json,"thumbnail")){
+	//var_dump($decoded["thumbnail"]["@id"]);
+	$urlParm = 'Location: ' . $decoded["thumbnail"]["@id"];
+	header($urlParm);
+	exit;	
+	}
+else{
+	echo "no thumbnail found for this object";
+	}
+?>
+
