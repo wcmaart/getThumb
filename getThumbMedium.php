655af47639f91d9d43558da4efd46b7a7027d24d
linguaphile <jallison@williams.edu>
2022-02-22 10:29:58 -0500

    Add files via upload

--- getThumbMedium.php
+++ getThumbMedium.php
@@ -1,18 +1,14 @@
 <?php
 $obID = $_GET["id"];
-$WCMAKey = getenv('WCMA_API_KEY');
+
 
 //echo $obID;
-echo $WCMAKey;
-
-
 $curl = curl_init();
 curl_setopt_array($curl,[
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://egallery.williams.edu/objects/' . $obID . '/json?key=' . $WCMAKey,
+    CURLOPT_URL => 'http://egallery/objects/' . $obID . '/json?key=f2878c3ce0518327ec2c5d9323957974f11742a29dd8ca24250ef73a743c5a88',
     CURLOPT_USERAGENT => 'egallery API in CURL'
 ]);
-
 $response = curl_exec($curl);
 
 //echo ($response);
@@ -22,19 +18,14 @@ $endURL = strpos($response,'/full',$startURL);
 //echo $endURL;
 $fullURL = substr($response,$startURL,$endURL - $startURL);
 //echo $fullURL;
-
-
 if (strpos($response,"primaryMedia")){
     
     $urlParm = 'Location: ' . $fullURL . '/thumbnail'; //bigger than postagestamp size
     header($urlParm);
-    curl_close();
     exit;
  }
 else{
     echo "no primary media found";
-    curl_close();
 }
- 
-    
+    curl_close();
 ?>
