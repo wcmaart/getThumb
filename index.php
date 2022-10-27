37f745753820a53c8dc8d981aa2328f03624f870
linguaphile <jallison@williams.edu>
2022-10-27 16:53:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
+<?php
+$myID =  $_GET["objectid"];
+echo $myID;
+
+//change $jsonurl to match domain
+$jsonurl = "https://rs.williams.edu/iiif/" . $myID . "/manifest";
+
+$json = file_get_contents($jsonurl);
+$decoded = json_decode($json,true);
+
+
+if (strpos($json,"thumbnail")){
+	//var_dump($decoded["thumbnail"]["@id"]);
+	$urlParm = 'Location: ' . $decoded["thumbnail"]["@id"];
+
+	header($urlParm);
+	
+	exit;	
+	}
+else{
+	echo "no thumbnail found for this object. 2";
+	}
+	
+?>
 
