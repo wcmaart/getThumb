6cecf308f2881e75d874041f068f375deb5b714e
linguaphile <jallison@williams.edu>
2022-11-13 11:12:40 -0500

    Update getThumb.php
    changed to resize the image to 150 pixels using iiif resize feature

--- getThumb.php
+++ getThumb.php
@@ -4,19 +4,18 @@ echo $myID;
 
 //change $jsonurl to match domain
 $jsonurl = "https://rs.williams.edu/iiif/" . $myID . "/manifest";
-echo $jsonurl;
-
 $json = file_get_contents($jsonurl);
 $decoded = json_decode($json,true);
 
+// str_replace changes the iiif thumbnail image to a smaller one using the iiif resizing feature
 if (strpos($json,"thumbnail")){
-	//var_dump($decoded["thumbnail"]["@id"]);
-	$urlParm = 'Location: ' . $decoded["thumbnail"]["@id"];
+	$urlParm = 'Location: ' . str_replace("thm","150,",$decoded["thumbnail"]["@id"]);
+
 	header($urlParm);
 	exit;	
 	}
 else{
-	echo "no thumbnail found for this object. 3";
+	echo "no thumbnail found for this object. 2";
 	}
 	
 ?>
